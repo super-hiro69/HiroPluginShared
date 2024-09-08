c8d68a9848698705f88827ab83b9cfedef16b3ea
Rex <Rex@PC-DE-PHILO>
2024-09-08 17:41:16 +0800

    添加项目文件。

---
+++ Modelviews/IHiroPlugin.cs
@@ -0,0 +1,21 @@
+﻿using Hiro.Plugin.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiro.Plugin.Modelviews
+{
+    public interface IHiroPlugin
+    {
+        string Id { get; } // 唯一标识符
+        string Author { get; } // 作者
+        string GetName(string language); // 根据语言返回插件名称
+        string GetDescription(string language); // 根据语言返回插件描述
+        void Initialize(IHiroService hostService); // 初始化插件并传递主程序服务
+        string HiroWeGo(string input, List<object>? para); // 处理请求的方法
+
+        bool Dispose();//释放所有资源, 卸载插件
+    }
+}
---
+++ Services/IHiroService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiro.Plugin.Services
+{
+    public interface IHiroService
+    {
+        void Log(string message, string? source);
+        string GetData(string key, List<object>? param);
+        bool RunExe(string path, string? source);
+    }
+}
