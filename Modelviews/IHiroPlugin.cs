02507848a38de9ab5be6181203457960126b4e23
禾生蓬中 <i@stud.im>
2024-09-23 21:30:14 +0800

    Interface Updated

--- Modelviews/IHiroPlugin.cs
+++ Modelviews/IHiroPlugin.cs
@@ -1,21 +1,72 @@
 ﻿using Hiro.Plugin.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Hiro.Plugin.Modelviews
 {
     public interface IHiroPlugin
     {
-        string Id { get; } // 唯一标识符
-        string Author { get; } // 作者
-        string GetName(string language); // 根据语言返回插件名称
-        string GetDescription(string language); // 根据语言返回插件描述
+        /// <summary>
+        /// 唯一标识符, 格式: com.domain.application.branch
+        /// </summary>
+        string? Id { get; }
+        /// <summary>
+        /// 作者, 格式: 任意文本, 256 字符以内
+        /// </summary>
+        string? Author { get; }
+        /// <summary>
+        /// 图标文件存放位置, 相对于 system/plugins/packageName/
+        /// </summary>
+        string? Icon { get; }
+        /// <summary>
+        /// 版本号
+        /// </summary>
+        HiroVersion? Version { get; }
+        /// <summary>
+        /// 是否允许自动加载, 允许自动加载且用户启用的插件将会在 Hiro 启动时自动加载
+        /// </summary>
+        bool? RunWithHiro {  get; }
+        /// <summary>
+        /// 是否在独立环境运行, 建议大型程序或资源占用较高的程序在独立环境运行, 以免造成主程序闪退
+        /// </summary>
+        bool? IsolateRun { get; }
+        /// <summary>
+        /// 基于当前语言获取名称
+        /// </summary>
+        /// <param name="language">当前语言代码</param>
+        /// <returns></returns>
+        string? GetName(string language);
+        /// <summary>
+        /// 基于当前语言获取描述
+        /// </summary>
+        /// <param name="language">当前语言代码</param>
+        /// <returns></returns>
+        string? GetDescription(string language);
+        /// <summary>
+        /// 插件首次被加载会触发此事件
+        /// </summary>
+        void FirstRun();
+        /// <summary>
+        /// 插件被更新后会触发此事件
+        /// </summary>
+        void UpdatedRun();
         void Initialize(IHiroService hostService); // 初始化插件并传递主程序服务
-        string HiroWeGo(string input, List<object>? para); // 处理请求的方法
-
-        bool Dispose();//释放所有资源, 卸载插件
+        /// <summary>
+        /// 主程序传递的请求方法, 正常请求完整请返回 'success'
+        /// </summary>
+        /// <param name="input">输入的 URL 或文件名</param>
+        /// <param name="para">额外附加的部分参数</param>
+        /// <returns></returns>
+        string? HiroWeGo(string input, List<object>? para);
+        /// <summary>
+        /// 主程序发出的系统消息, 类如 用户登出, 用户名改变等
+        /// </summary>
+        /// <param name="procID">消息 ID</param>
+        /// <param name="para">额外附加的部分参数</param>
+        /// <returns></returns>
+        bool? HiroWndProc(int procID, List<object>? para);
+        /// <summary>
+        /// 释放所有资源, 卸载插件
+        /// </summary>
+        /// <returns></returns>
+        bool? Dispose();
     }
 }
--- Services/IHiroService.cs
+++ Services/IHiroService.cs
@@ -1,15 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Hiro.Plugin.Modelviews;
 
 namespace Hiro.Plugin.Services
 {
     public interface IHiroService
     {
-        void Log(string message, string? source);
-        string GetData(string key, List<object>? param);
-        bool RunExe(string path, string? source);
+        /// <summary>
+        /// 输出日志内容到日志文件
+        /// </summary>
+        /// <param name="message">日志文本</param>
+        /// <param name="source">来源</param>
+        void Log(string message, string? source = null);
+        /// <summary>
+        /// 输出错误信息到日志文件
+        /// </summary>
+        /// <param name="ex">错误信息</param>
+        /// <param name="Module">模块名称, 方便定位错误</param>
+        void LogError(Exception ex, string Module);
+        /// <summary>
+        /// 获取指定 Key 值的数据
+        /// </summary>
+        /// <param name="key">Key 值</param>
+        /// <param name="param">附加参数</param>
+        /// <returns></returns>
+        List<object>? GetData(string key, List<object>? param = null);
+        /// <summary>
+        /// 调用主程序运行文件
+        /// </summary>
+        /// <param name="path">运行路径</param>
+        /// <param name="source">来源</param>
+        /// <returns></returns>
+        bool? RunExe(string path, string? source);
+        /// <summary>
+        /// 关联指定协议或拓展名到插件
+        /// </summary>
+        /// <param name="protocol">协议或拓展名, 示例: "https:", ".txt"</param>
+        /// <param name="RunParam">运行方式, 示例: "hiro(%1)"</param>
+        /// <returns></returns>
+        bool? Link(string protocol, string RunParam);
+        /// <summary>
+        /// 托管主程序发送通知
+        /// </summary>
+        /// <param name="i">要发送的通知内容</param>
+        void Notify(Hiro_Notice i);
+        /// <summary>
+        /// 读取设置内容
+        /// </summary>
+        /// <param name="Section">设置节标题</param>
+        /// <param name="Key">设置键</param>
+        /// <param name="defaultText">默认内容</param>
+        /// <returns></returns>
+        string ReadConfig(string Section,string Key, string defaultText);
     }
 }
